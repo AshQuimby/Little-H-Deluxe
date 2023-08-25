e0617084d9b986a5db88bb8c21aaeb6a39e32a89
AshQuimby <ian.aaron.mcallister@gmail.com>
2023-08-24 18:18:43 -0700

    Fixing crash on MacOS

--- core/src/com/sab/littleh/game/level/Level.java
+++ core/src/com/sab/littleh/game/level/Level.java
@@ -599,7 +599,7 @@ public class Level {
     }
 
     public void drawPostRenders(Graphics g, List<Tile> postRenders) {
-//        waterShader.setUniformf("u_tick", gameTick / 60f);
+        waterShader.setUniformf("u_tick", gameTick / 60f);
         for (Tile tile : postRenders) {
             if (tile.hasTag("render_normal")) {
                 if (tile.hasTag("water")) {
